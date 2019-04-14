0cd401f54995e6be4e8c23d3e71cb9cfcdb57a21
Administrator <soilwear@163.com>
2019-04-14 10:14:58 +0800

    druid 及监控 的demo     具体配置

---
+++ src/main/java/com/druid/demo/druid/DataSourceProperties.java
@@ -0,0 +1,74 @@
+package com.druid.demo.druid;
+
+import javax.sql.DataSource;
+import com.alibaba.druid.pool.DruidDataSource;
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+import java.sql.SQLException;
+import java.util.Properties;
+
+@Component
+@Configuration
+@PropertySource("classpath:application.yml")
+@ConfigurationProperties(prefix = "spring.datasource")
+@Data
+public class DataSourceProperties {
+
+    private String driverClassName;
+    private String url;
+    private String username;
+    private String password;
+
+    private int initialSize;
+    private int minIdle;
+    private int maxActive;
+    private int maxWait;
+    private int timeBetweenEvictionRunsMillis;
+    private int minEvictableIdleTimeMillis;
+    private String validationQuery;
+    private boolean testWhileIdle;
+    private boolean testOnBorrow;
+    private boolean testOnReturn;
+    private boolean poolPreparedStatements;
+    private int maxPoolPreparedStatementPerConnectionSize;
+    private String filters;
+    private Properties connectionProperties;
+
+    @Bean //声明为Bean实例对象
+    @Primary //在同样的DataSource中优先使用被标注的DataSource
+    public DataSource dataSource() {
+
+        DruidDataSource dataSource = new DruidDataSource();
+        dataSource.setUrl(url);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+        dataSource.setDriverClassName(driverClassName);
+
+        //Configuration
+        dataSource.setInitialSize(initialSize);
+        dataSource.setMinIdle(minIdle);
+        dataSource.setMaxActive(maxActive);
+        dataSource.setMaxWait(maxWait);
+        dataSource.setTimeBetweenEvictionRunsMillis(timeBetweenEvictionRunsMillis);
+        dataSource.setMinEvictableIdleTimeMillis(minEvictableIdleTimeMillis);
+        dataSource.setValidationQuery(validationQuery);
+        dataSource.setTestWhileIdle(testWhileIdle);
+        dataSource.setTestOnBorrow(testOnBorrow);
+        dataSource.setTestOnReturn(testOnReturn);
+        dataSource.setPoolPreparedStatements(poolPreparedStatements);
+        dataSource.setMaxPoolPreparedStatementPerConnectionSize(maxPoolPreparedStatementPerConnectionSize);
+        try {
+            dataSource.setFilters(filters);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        dataSource.setConnectProperties(connectionProperties);
+        return dataSource;
+    }
+}
